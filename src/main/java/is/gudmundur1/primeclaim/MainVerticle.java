8f96767f73e8cc15275498b2cd8b2457997d8bfe
Gudmundur Orn Johannsson <gudmundur.orn@gmail.com>
2019-03-31 15:18:35 +0000

    Improve readability by putting (nested) error handling in front of success case

--- src/main/java/is/gudmundur1/primeclaim/MainVerticle.java
+++ src/main/java/is/gudmundur1/primeclaim/MainVerticle.java
@@ -7,7 +7,6 @@ import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.asyncsql.PostgreSQLClient;
 import io.vertx.ext.sql.SQLClient;
-import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
@@ -29,26 +28,24 @@ public class MainVerticle extends AbstractVerticle {
 
     Router router = Router.router(vertx);
     router.route(HttpMethod.GET, "/").handler(routingContext -> {
-      sqlClient.getConnection(res -> {
-        if (res.succeeded()) {
-
-          SQLConnection connection = res.result();
-
-          // Got a connection
-          connection.query("select 'hello' as col1" , event -> {
-            String fromDatabase = event.result().getRows().get(0).getString("col1");
-            routingContext.response()
-              .putHeader("content-type", "text/plain")
-              .end(fromDatabase);
-          });
-
-        } else {
-          // Failed to get connection - deal with it
-          LOGGER.error("Exception when getting connection", res.cause());
+      sqlClient.getConnection(getConnection -> {
+        if (getConnection.failed()) {
+          LOGGER.error("Exception when getting connection", getConnection.cause());
           fail(routingContext);
+          return;
         }
+        getConnection.result().query("select 'hello' as col1", query -> {
+          if (query.failed()) {
+            LOGGER.error("Query failed", query.cause());
+            fail(routingContext);
+            return;
+          }
+          String fromDatabase = query.result().getRows().get(0).getString("col1");
+          routingContext.response()
+            .putHeader("content-type", "text/plain")
+            .end(fromDatabase);
+          });
       });
-
     });
     // TODO add pagination
     // TODO add filter by owner
@@ -56,92 +53,74 @@ public class MainVerticle extends AbstractVerticle {
       HttpServerResponse response = routingContext.response();
       response.putHeader("content-type", "text/plain").setChunked(true);
 
-      sqlClient.getConnection(res -> {
-        if (res.succeeded()) {
-
-          SQLConnection connection = res.result();
-
-          // Got a connection
-          connection.query("select prime, owner from claim" , event -> {
-            if (event.succeeded()) {
-              event.result().getRows().forEach(x -> {
-                response.write(x.toString());
-              });
-              response.end();
-            } else {
-              LOGGER.error("Exception in get claims", event.cause());
-              fail(routingContext);
-            }
-          });
-
-        } else {
-          // Failed to get connection - deal with it
-          LOGGER.error("Exception when getting connection", res.cause());
+      sqlClient.getConnection(getConnection -> {
+        if (getConnection.failed()) {
+          LOGGER.error("Exception when getting connection", getConnection.cause());
           fail(routingContext);
+          return;
         }
+        getConnection.result().query("select prime, owner from claim" , query -> {
+          if (query.failed()) {
+            LOGGER.error("Exception in get claims", query.cause());
+            fail(routingContext);
+            return;
+          }
+          query.result().getRows().forEach(x -> {
+            response.write(x.toString());
+          });
+          response.end();
+        });
       });
 
     });
     router.route(HttpMethod.GET, "/createtestclaims").handler(routingContext -> {
-      sqlClient.getConnection(res -> {
-        if (res.succeeded()) {
-
-          SQLConnection connection = res.result();
-
-          // Got a connection
-          connection.execute("insert into claim (prime, owner) values (1, 'bob')", event -> {
-            if (event.succeeded()) {
-              routingContext.response()
-                .putHeader("content-type", "text/plain")
-                .end("ok");
-            } else {
-              LOGGER.error("Exception in create test claim", event.cause());
-              fail(routingContext);
-            }
-          });
-
-        } else {
-          // Failed to get connection - deal with it
-          LOGGER.error("Exception when getting connection", res.cause());
+      sqlClient.getConnection(getConnection -> {
+        if (getConnection.failed()) {
+          LOGGER.error("Exception when getting connection", getConnection.cause());
+          fail(routingContext);
+          return;
         }
+        getConnection.result().execute("insert into claim (prime, owner) values (1, 'bob')", execute -> {
+          if (execute.failed()) {
+            LOGGER.error("Exception in create test claim", execute.cause());
+            fail(routingContext);
+            return;
+          }
+          routingContext.response()
+            .putHeader("content-type", "text/plain")
+            .end("ok");
+        });
       });
-
     });
     router.route(HttpMethod.GET, "/ping").handler(routingContext -> {
-      sqlClient.getConnection(res -> {
-        if (res.succeeded()) {
-
-          SQLConnection connection = res.result();
-
-          // Got a connection
-          connection.query("select 'pong' as col1" , event -> {
-            if (event.succeeded()) {
-              String fromDatabase = event.result().getRows().get(0).getString("col1");
-              routingContext.response()
-                .putHeader("content-type", "text/plain")
-                .end(fromDatabase);
-            } else {
-              LOGGER.error("Exception executing query", event.cause());
-              fail(routingContext);
-            }
-          });
-
-        } else {
-          // Failed to get connection - deal with it
-          LOGGER.error("Exception when getting connection", res.cause());
+      sqlClient.getConnection(getConnection -> {
+        if (getConnection.failed()) {
+          LOGGER.error("Exception when getting connection", getConnection.cause());
           fail(routingContext);
+          return;
         }
+        getConnection.result().query("select 'pong' as col1" , event -> {
+          if (event.failed()) {
+            LOGGER.error("Exception executing query", event.cause());
+            fail(routingContext);
+            return;
+          }
+          String fromDatabase = event.result().getRows().get(0).getString("col1");
+          routingContext.response()
+            .putHeader("content-type", "text/plain")
+            .end(fromDatabase);
+        });
       });
     });
 
 
     vertx.createHttpServer().requestHandler(router).listen(8888, http -> {
-      if (http.succeeded()) {
-        startFuture.complete();
-        LOGGER.info("HTTP server started on port 8888");
-      } else {
+      if (http.failed()) {
         startFuture.fail(http.cause());
+        return;
       }
+      startFuture.complete();
+      LOGGER.info("HTTP server started on port 8888");
     });
   }
 
