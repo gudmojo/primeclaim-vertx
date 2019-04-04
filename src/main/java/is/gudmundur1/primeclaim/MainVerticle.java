e341130e723d2c469b90f77267047c206c8e9d36
Gudmundur Orn Johannsson <gudmundur.orn@gmail.com>
2019-04-04 12:21:32 +0000

    Convert the project from callbacks to RxJava

--- src/main/java/is/gudmundur1/primeclaim/MainVerticle.java
+++ src/main/java/is/gudmundur1/primeclaim/MainVerticle.java
@@ -1,22 +1,18 @@
 package is.gudmundur1.primeclaim;
 
-import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.asyncsql.PostgreSQLClient;
-import io.vertx.ext.sql.SQLClient;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.http.HttpServerResponse;
+import io.vertx.reactivex.ext.asyncsql.PostgreSQLClient;
+import io.vertx.reactivex.ext.sql.SQLClient;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.handler.BodyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.security.SecureRandom;
-import java.util.List;
-import java.util.Random;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -34,152 +30,109 @@ public class MainVerticle extends AbstractVerticle {
 
     Router router = Router.router(vertx);
     router.route().handler(BodyHandler.create());
-    router.route(HttpMethod.GET, "/").handler(routingContext -> {
-      sqlClient.getConnection(getConnection -> {
-        if (getConnection.failed()) {
-          LOGGER.error("Exception when getting connection", getConnection.cause());
-          fail(routingContext);
-          return;
-        }
-        getConnection.result().query("select 'hello' as col1", query -> {
-          if (query.failed()) {
-            LOGGER.error("Query failed", query.cause());
+    router.route(HttpMethod.GET, "/").handler(routingContext ->
+      sqlClient.rxGetConnection().flatMap(connection ->
+        connection.rxQuery("select 'hello' as col1").doAfterTerminate(connection::close))
+          .subscribe(resultSet -> {
+            // Send JSON to the client
+            routingContext.response()
+              .putHeader("content-type", "text/plain")
+              .end(resultSet.getRows().get(0).getString("col1"));
+          }, t -> {
+            // Send error to the client
+            LOGGER.error("Query failed", t);
             fail(routingContext);
-            return;
-          }
-          String fromDatabase = query.result().getRows().get(0).getString("col1");
-          routingContext.response()
-            .putHeader("content-type", "text/plain")
-            .end(fromDatabase);
-          });
-      });
-    });
+          }));
+
     // TODO add pagination
     // TODO add filter by owner
     router.route(HttpMethod.GET, "/claims").handler(routingContext -> {
       HttpServerResponse response = routingContext.response();
       response.putHeader("content-type", "text/plain").setChunked(true);
 
-      sqlClient.getConnection(getConnection -> {
-        if (getConnection.failed()) {
-          LOGGER.error("Exception when getting connection", getConnection.cause());
-          fail(routingContext);
-          return;
-        }
-        getConnection.result().query("select prime, owner from claim" , query -> {
-          if (query.failed()) {
-            LOGGER.error("Exception in get claims", query.cause());
-            fail(routingContext);
-            return;
-          }
-          query.result().getRows().forEach(x -> {
+      sqlClient.rxGetConnection().flatMap(connection ->
+        connection.rxQuery("select prime, owner from claim").doAfterTerminate(connection::close))
+        .subscribe(resultSet -> {
+          resultSet.getRows().forEach(x -> {
             response.write(x.toString());
           });
           response.end();
-        });
-      });
-
-    });
-    router.route(HttpMethod.GET, "/createtestclaims").handler(routingContext -> {
-      sqlClient.getConnection(getConnection -> {
-        if (getConnection.failed()) {
-          LOGGER.error("Exception when getting connection", getConnection.cause());
+        }, err -> {
+          LOGGER.error("Query failed", err);
           fail(routingContext);
-          return;
-        }
-        getConnection.result().execute("insert into claim (prime, owner) values (1, 'bob')", execute -> {
-          if (execute.failed()) {
-            LOGGER.error("Exception in create test claim", execute.cause());
-            fail(routingContext);
-            return;
-          }
+        });
+    });
+
+
+    router.route(HttpMethod.GET, "/createtestclaims").handler(routingContext ->
+      sqlClient.rxGetConnection().flatMap(connection ->
+        connection.rxUpdate("insert into claim (prime, owner) values (1, 'bob')").doAfterTerminate(connection::close))
+        .subscribe(result -> {
           routingContext.response()
             .putHeader("content-type", "text/plain")
             .end("ok");
-        });
-      });
-    });
-    router.route(HttpMethod.GET, "/ping").handler(routingContext -> {
-      sqlClient.getConnection(getConnection -> {
-        if (getConnection.failed()) {
-          LOGGER.error("Exception when getting connection", getConnection.cause());
+        }, err -> {
+          LOGGER.error("Exception in create test claim", err);
           fail(routingContext);
-          return;
-        }
-        getConnection.result().query("select 'pong' as col1" , event -> {
-          if (event.failed()) {
-            LOGGER.error("Exception executing query", event.cause());
-            fail(routingContext);
-            return;
-          }
-          String fromDatabase = event.result().getRows().get(0).getString("col1");
+        }));
+
+    router.route(HttpMethod.GET, "/ping").handler(routingContext ->
+      sqlClient.rxGetConnection().flatMap(connection ->
+        connection.rxQuery("select 'pong' as col1").doAfterTerminate(connection::close))
+        .subscribe(result -> {
           routingContext.response()
             .putHeader("content-type", "text/plain")
-            .end(fromDatabase);
-        });
-      });
-    });
+            .end(result.getRows().get(0).getString("col1"));
+        }, err -> {
+          LOGGER.error("Exception executing query", err);
+          fail(routingContext);
+        }));
+
     router.route(HttpMethod.POST, "/user").handler(routingContext -> {
       JsonObject bodyAsJson = routingContext.getBodyAsJson();
       String username = bodyAsJson.getString("username");
-      // TODO: verify username is valid and not null
-      sqlClient.getConnection(getConnection -> {
-        if (getConnection.failed()) {
-          LOGGER.error("Exception when getting connection", getConnection.cause());
-          fail(routingContext);
-          return;
-        }
-        JsonArray params = new JsonArray();
-        params.add(username);
-        params.add(bodyAsJson.getBoolean("isadmin"));
-        params.add(ApiKeyUtil.generateApiKey());
-        String sql =
-          " with ins1 as ( " +
+      JsonArray params = new JsonArray();
+      params.add(username);
+      params.add(bodyAsJson.getBoolean("isadmin"));
+      params.add(ApiKeyUtil.generateApiKey());
+      String sql =
+        " with ins1 as ( " +
           "   insert into appuser (username, isadmin) values (?, ?) " +
           "   returning id as user_id " +
           " )" +
           " insert into apikey (apikey, userid) " +
           " select ?, user_id from ins1;";
-        getConnection.result().updateWithParams(sql, params, execute -> {
-          if (execute.failed()) {
-            LOGGER.error("Exception executing insert", execute.cause());
+      // TODO: verify username is valid and not null
+      sqlClient.rxGetConnection().flatMap(connection ->
+        connection.rxUpdateWithParams(sql, params).doAfterTerminate(connection::close))
+        .subscribe(result -> {
+            LOGGER.info("Insert success");
+            routingContext.response().end();
+          }, err -> {
+            LOGGER.error("Exception executing insert", err);
             fail(routingContext);
-            return;
-          }
-          LOGGER.info("Insert success");
-          routingContext.response().end();
-        });
-      });
+          });
     });
+
     router.route(HttpMethod.GET, "/user/:username").handler(routingContext -> {
       HttpServerResponse response = routingContext.response();
+      String sql =
+        "select username, isadmin, apikey " +
+          " from appuser u left join apikey on u.id = userid " +
+          " where username = ?";
+      JsonArray params = new JsonArray();
+      String username = routingContext.request().getParam("username");
+      params.add(username);
 
-      sqlClient.getConnection(getConnection -> {
-        if (getConnection.failed()) {
-          LOGGER.error("Exception when getting connection", getConnection.cause());
+      sqlClient.rxGetConnection().flatMap(connection ->
+        connection.rxQueryWithParams(sql, params).doAfterTerminate(connection::close))
+        .subscribe(result -> {
+          response.end(result.getRows().get(0).encode());
+        }, err -> {
+          LOGGER.error("Exception in get user", err);
           fail(routingContext);
-          return;
-        }
-        String sql =
-          "select username, isadmin, apikey " +
-            " from appuser u left join apikey on u.id = userid " +
-            " where username = ?";
-        JsonArray params = new JsonArray();
-        String username = routingContext.request().getParam("username");
-        params.add(username);
-        getConnection.result().queryWithParams(sql, params, query -> {
-          if (query.failed()) {
-            LOGGER.error("Exception in get user", query.cause());
-            fail(routingContext);
-            return;
-          }
-          List<JsonObject> rows = query.result().getRows();
-          String responseText = rows.get(0).encode();
-          response.end(responseText);
         });
-      });
     });
-
 
     Integer httpPort = config().getInteger("http.port");
     vertx.createHttpServer().requestHandler(router).listen(httpPort, http -> {
