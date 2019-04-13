151a9225f69d4708b011e7cf844ee634296123fc
Gudmundur Orn Johannsson <gudmundur.orn@gmail.com>
2019-04-13 19:48:08 +0000

    json keys are now camel case

--- src/main/java/is/gudmundur1/primeclaim/ConfigKey.java
+++ src/main/java/is/gudmundur1/primeclaim/ConfigKey.java
@@ -1,11 +1,11 @@
 package is.gudmundur1.primeclaim;
 
 public class ConfigKey {
-  public static final String POSTGRES_HOST = "postgres-host";
-  public static final String POSTGRES_PORT = "postgres-port";
-  public static final String POSTGRES_DATABASE = "postgres-database";
-  public static final String POSTGRES_USER = "postgres-user";
-  public static final String POSTGRES_PASSWORD = "postgres-password";
-  public static final String BOOTSTRAP_ADMIN_API_KEY = "bootstrapadminkey";
-  public static final String LISTEN_PORT = "http.port";
+  public static final String POSTGRES_HOST = "postgresHost";
+  public static final String POSTGRES_PORT = "postgresPort";
+  public static final String POSTGRES_DATABASE = "postgresDatabase";
+  public static final String POSTGRES_USER = "postgresUser";
+  public static final String POSTGRES_PASSWORD = "postgresPassword";
+  public static final String BOOTSTRAP_ADMIN_API_KEY = "bootstrapAdminKey";
+  public static final String LISTEN_PORT = "httpPort";
 }
